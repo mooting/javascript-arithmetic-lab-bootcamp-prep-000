74026773bbe735712ac7252efc0efc48a615454b
Justin Davis <justin_w_davis@yahoo.com>
2018-10-26 03:06:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
-//function add(a, b) { return a + b }
+
 const add = ( a, b ) => a + b
+
+const subtract = ( a, b ) => a - b
+
+const multiply = ( a, b ) => a * b
+
+const divide = ( a, b ) => a / b
+
+const inc = ( n ) => n++
+
+const dec = ( n ) => n--
